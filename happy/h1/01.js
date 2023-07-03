7760671e3a485f4262ecbf3b5d30d4dd49935c6b
1300fardowsa <fardowsahussein2008@gmail.com>
2023-07-03 15:56:59 +0300

    intial commit

---
+++ happy/h1/01.js
@@ -0,0 +1,14 @@
+let username ="fardowsa"
+let number = 123
+let condition = false
+
+console.log(username)
+
+let school_name // variable declaration
+console.log(school_name)
+school_name = "USIU" // assignment
+console.log(school_name)
+
+console.log(typeof(school_name))
+console.log(typeof(number))
+console.log(typeof(condition))
---
+++ happy/h1/hello.js
@@ -0,0 +1,9 @@
+// console.log("hello world")
+// console.log("hello")
+let userName ="fardowsa"
+console.log(userName)
+userName = "abc"
+console.log(userName)
+
+const pie = 3.142 // const - constant variable
+
